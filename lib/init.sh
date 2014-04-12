e693542f5ed4bf76a88c913d0b56f03ee25bed5c
Michael Lieberman <mike.d.lieberman@gmail.com>
2014-04-11 23:08:41 -0400

    Added main script, lib init and bin stubs

---
+++ lib/init.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# Cygwin check.
+if [[ "$(uname -s)" == CYGWIN* ]] ; then
+    TEXTY_CYGWIN=1
+fi
+cygcheck() {
+    [ $TEXTY_CYGWIN ]
+}
+
+# Fix up Cygwin paths.
+cygfix() {
+    cygcheck && echo "$(cygpath -w $@)" || echo "$@"
+}
+
+# Run tika.
+TIKA_PATH=$(cygfix "$TEXTY_HOME/lib/tika-app-1.5.jar")
+texty_tika() {
+    java -jar "$TIKA_PATH" "$@"
+}
+
+# Run OpenNLP.
+OPENNLP_PATH=$(cygfix "$TEXTY_HOME/lib/apache-opennlp-1.5.3/bin/opennlp")
+# OpenNLP needs the batch version if in Cygwin.
+cygcheck && OPENNLP_PATH="$OPENNLP_PATH.bat"
+texty_opennlp() {
+    "$OPENNLP_PATH" "$@"
+}
+
+# Run Mallet.
+export MALLET_HOME="$(cygfix $TEXTY_HOME/lib/mallet-2.0.7)"
+MALLET_PATH="$MALLET_HOME/bin/mallet"
+# Needs batch version if in Cygwin.
+cygcheck && MALLET_PATH="$MALLET_PATH.bat"
+texty_mallet() {
+    "$MALLET_PATH" "$@"
+}
+
